b1c706be27f7ab74afc719db744ef818e551e395
Kaixu <zhangkaixu@hotmail.com>
2012-06-20 17:34:01 +0800

    bugfix for the script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-W=$0
-cs_path=${W%/*}
-cd
-echo "这个程序用来给新安装的Linux机器做一些自动的配置"
+cs_path=`pwd`
+echo ${cs_path}
+echo "这个脚本用来给新安装的Linux机器做一些自动配置工作"
 echo "处理vim配置文件 .vim/目录和.vimrc文件"
-rm .vim -r -f
-ln -s ${cs_path}/_vim .vim
-rm .vimrc
-ln -s ${cs_path}/_vimrc .vimrc
+rm ~/.vim -r -f
+ln -s ${cs_path}/_vim ~/.vim
+rm ~/.vimrc
+ln -s ${cs_path}/_vimrc ~/.vimrc
 echo "配置完成"
