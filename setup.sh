182fcbf8cf82391f041a144df9bc21e27efb74c9
Kaixu <zhangkaixu@hotmail.com>
2012-06-22 17:50:18 +0800

    add bash_profile

--- setup.sh
+++ setup.sh
@@ -5,9 +5,13 @@ echo "这个脚本用来给新安装的Linux机器做一些自动配置工作"
 echo "处理vim配置文件 .vim/目录和.vimrc文件"
 rm ~/.vim -r -f
 ln -s ${cs_path}/_vim ~/.vim
-rm ~/.vimrc
+rm ~/.vimrc -f
 ln -s ${cs_path}/_vimrc ~/.vimrc
 
-rm ~/.tmux.conf
+rm ~/.tmux.conf -f
 ln -s ${cs_path}/.tmux.conf ~/.tmux.conf
+
+
+rm ~/.bash_profile -f
+ln -s ${cs_path}/.bash_profile ~/.bash_profile
 echo "配置完成"
